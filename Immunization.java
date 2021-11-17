658c14a72e64d88b64bc4597ec85418845e3e2e6
CruiseC <90277160+CruiseC@users.noreply.github.com>
2021-11-17 12:21:05 -0700

    Update Immunization.java

--- Immunization.java
+++ Immunization.java
@@ -2,19 +2,19 @@ package PatientLayers;
 
 import java.util.Date;
 
-public class Immunization {
+public class Immunization {						//Immunization class is going to be public
 	private String name;
 	private String dosage;
 	private Date day;
-	public Immunization(String name, String dosage, Date day)
+	public Immunization(String name, String dosage, Date day)	
 	{
 		this.name = name;
 		this.dosage = dosage;
 		this.day = day;
 		
 	}
-	public String toString()
+	public String toString()					//string to print out return statement
 	{
-		return name + " " + dosage + " " + day.toString();
+		return name + " " + dosage + " " + day.toString();	
 	}
 }
