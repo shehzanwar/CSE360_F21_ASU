425f8f2bcc7617d040a90afc62e5f5e9a0201227
CruiseC <90277160+CruiseC@users.noreply.github.com>
2021-11-17 12:28:55 -0700

    Update Medication.java

--- Medication.java
+++ Medication.java
@@ -1,15 +1,15 @@
 package PatientLayers;
 import java.util.Date;
 
-public class Medication {
+public class Medication {										//Medication class is set as Public
 	private String name;
 	private String dosage;
 	private Date startDate;
 	private Date endDate;
 	
-	public Medication(String name, String dosage, Date startDate, Date endDate)
+	public Medication(String name, String dosage, Date startDate, Date endDate)			//Medication method
 	{
-		this.name = name;
+		this.name = name;									//
 		this.dosage = dosage;
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -20,8 +20,8 @@ public class Medication {
 		return endDate;
 	}
 	
-	public String toString()
+	public String toString()									
 	{
-		return name + " " + dosage + " " + startDate.toString() + " " + endDate.toString();
+		return name + " " + dosage + " " + startDate.toString() + " " + endDate.toString();	//string to print out our return statement
 	}
 }
